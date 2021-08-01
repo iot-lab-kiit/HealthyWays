e027e91cc1d1a3b4eb91d37d6d1344e464904a62
Siddharth Singh Baghel <siddharthsinghrewa5@gmail.com>
2021-08-02 03:16:00 +0530

    IW History Entity

---
+++ app/src/main/java/com/siddharthsinghbaghel/healthyways/room/iWHistory/IWHistoryEntity.kt
@@ -0,0 +1,13 @@
+package com.siddharthsinghbaghel.healthyways.room.iWHistory
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "IwHistory")
+data class IWHistoryEntity(@ColumnInfo(name = "IW Value") var IWValue: String,
+                    @ColumnInfo(name = "IW Weight") var iwWeight: String,
+                    @ColumnInfo(name = "IW Height") var iwHeight: String )
+                    {
+    @PrimaryKey(autoGenerate = true) var id = 0
+}
