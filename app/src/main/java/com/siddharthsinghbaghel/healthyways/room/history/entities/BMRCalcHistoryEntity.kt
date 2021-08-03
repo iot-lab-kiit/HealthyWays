772a96c3b89740bc67acc75781b4c278c91af535
Siddharth Singh Baghel <siddharthsinghrewa5@gmail.com>
2021-08-03 18:03:09 +0530

    Histories layout files

--- app/src/main/java/com/siddharthsinghbaghel/healthyways/room/history/entities/BMRCalcHistoryEntity.kt
+++ app/src/main/java/com/siddharthsinghbaghel/healthyways/room/history/entities/BMRCalcHistoryEntity.kt
@@ -6,11 +6,11 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "BMRCalcHistory")
 data class BMRCalcHistoryEntity(@ColumnInfo(name = "Exercise Extent") var exerExtent: String,
-                                @ColumnInfo(name = "Age") var age: String,
                                 @ColumnInfo(name = "Weight") var weightValue: String,
                                 @ColumnInfo(name = "Height") var heightValue: String,
                                 @ColumnInfo(name = "BMR DateTime") var bmrDateT: String,
-                                @ColumnInfo(name = "BMR Value") var bmrValue: String,)
+                                @ColumnInfo(name = "BMR Value") var bmrValue: String,
+                                @ColumnInfo(name = "TDEE Value") var tdeeValue: String,)
 
 {
     @PrimaryKey(autoGenerate = true) var id = 0
