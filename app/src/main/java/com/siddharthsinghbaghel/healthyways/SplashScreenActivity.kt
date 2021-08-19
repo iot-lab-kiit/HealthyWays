89063114c246d8e9a3736ca661e5e75878af2d1a
Siddharth Singh Baghel <siddharthsinghrewa5@gmail.com>
2021-08-19 12:07:29 +0530

    Splash Screen

---
+++ app/src/main/java/com/siddharthsinghbaghel/healthyways/SplashScreenActivity.kt
@@ -0,0 +1,19 @@
+package com.siddharthsinghbaghel.healthyways
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivityBottomNav::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}
