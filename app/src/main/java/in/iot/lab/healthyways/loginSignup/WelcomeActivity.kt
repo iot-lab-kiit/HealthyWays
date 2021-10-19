8f940a86dba9f1be060c4c873b78153d7ae90121
Siddharth Singh Baghel <siddharthsinghrewa5@gmail.com>
2021-10-19 21:16:10 +0530

    New Welcome page

---
+++ app/src/main/java/in/iot/lab/healthyways/loginSignup/WelcomeActivity.kt
@@ -0,0 +1,18 @@
+package `in`.iot.lab.healthyways.loginSignup
+
+import `in`.iot.lab.healthyways.R
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class WelcomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+    }
+
+    fun openLoginIntent(view: View) {
+
+
+    }
+}
